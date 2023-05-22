af690b3ef5d88fe5d7ee3ee4821f36d3b7cf8edb
seunggyun-jeong <jskyun98@gmail.com>
2023-05-22 18:15:12 +0900

    [INIT] initial commit

---
+++ HealthPT-iOS/HealthPT_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthPT_iOSApp.swift
+//  HealthPT-iOS
+//
+//  Created by 정승균 on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthPT_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
